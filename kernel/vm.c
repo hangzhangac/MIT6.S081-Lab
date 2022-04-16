7660d11aa9d7c188a7748e83815b49d7261d418c
Hang Zhang <hz2447@nyu.edu>
2022-04-16 03:17:36 +0000

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+// Hang Zhang: print the pagetable recurcively.
+void
+vmprint(pagetable_t pagetable, uint64 depth){
+  if(depth > 2) return;
+  if(depth == 0){
+    printf("page table %p\n",pagetable);
+  }
+  char buf[12]={'.','.','\0'};
+  int d = depth, buf_id = 2;
+  while(d--){
+    buf[buf_id] = ' ';
+    buf[buf_id+1] = '.';
+    buf[buf_id+2] = '.';
+    buf[buf_id+3] = '\0';
+    buf_id+=3;
+  }
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("%s%d: pte %p pa %p\n",buf, i, pte, PTE2PA(pte));
+      uint64 child = PTE2PA(pte);
+      vmprint((pagetable_t)child, depth+1);
+    } 
+  }
+  return;
+}
