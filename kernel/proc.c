b004d42a607009f62e95f6ae2b0e4d0b01c360ca
Hang Zhang <hz2447@nyu.edu>
2022-05-08 02:24:34 +0000

    Alarm(Hard)

--- kernel/proc.c
+++ kernel/proc.c
@@ -140,6 +140,8 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p->ticks = 0;
+  p->handler_executing = 0;
 
   return p;
 }
